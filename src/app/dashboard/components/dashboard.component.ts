242aaa46ad038c6586cb8a728e773fa4fedb35da
siancafi1 <sacastanedafr@ut.edu.co>
2020-05-16 19:14:48 -0500

    dashboart al 50%

--- src/app/dashboard/components/dashboard.component.ts
+++ src/app/dashboard/components/dashboard.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  carts1 = [
+    {'number': 15 ,'descrition': 'ejemplo', 'class':'success'},
+    {'number': 50 ,'descrition': 'ejemplo', 'class':'warning'},
+    {'number': 80 ,'descrition': 'ejemplo', 'class':'primary'}
+  ];
 
+  carts2 = [
+    {'number': 523 ,'title': 'ejemplo', 'class':'primary','codigo':'TV', 'icon':'glyphicon glyphicon-eye-open'},
+    {'number': 81 ,'title': 'ejemplo', 'class':'danger','codigo':'TI', 'icon':''},
+    {'number': 21 ,'title': 'ejemplo', 'class':'success','codigo':'OI', 'icon':''},
+    {'number': 60 ,'title': 'ejemplo', 'class':'warning','codigo':'CI', 'icon':''}
+  ];
   constructor() { }
 
   ngOnInit(): void {
